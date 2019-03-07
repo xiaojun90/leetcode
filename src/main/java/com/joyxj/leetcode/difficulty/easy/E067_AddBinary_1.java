938caa76ea544f797a253958dab12aa54f776f02
xiaojun90 <838249367@qq.com>
2019-03-07 12:33:47 +0800

    [67] 二进制求和

--- src/main/java/com/joyxj/leetcode/difficulty/easy/E067_AddBinary_1.java
+++ src/main/java/com/joyxj/leetcode/difficulty/easy/E067_AddBinary_1.java
@@ -1,4 +1,7 @@
 package com.joyxj.leetcode.difficulty.easy;
+
+import java.util.Stack;
+
 /*
  * @lc app=leetcode.cn id=67 lang=java
  *
@@ -27,7 +30,7 @@ package com.joyxj.leetcode.difficulty.easy;
  * 输出: "10101"
  * 
  */
-public class E067_AddBinary {
+public class E067_AddBinary_1 {
 
     public static void main(String[] args) {
         String a = "1010";
@@ -38,35 +41,33 @@ public class E067_AddBinary {
 
     class Solution {
         public String addBinary(String a, String b) {
-            int max = Math.max(a.length(), b.length());
-            // 补齐空缺的位
-            if (a.length() > b.length()) {
-                StringBuffer s1 = new StringBuffer();
-                for (int i=0;i<a.length() - b.length();i++) {
-                    s1.append("0");
-                }
-                b = s1.toString() + b;
-            }
-            if (a.length() < b.length()) {
-                StringBuffer s1 = new StringBuffer();
-                for (int i=0;i<b.length() - a.length();i++) {
-                    s1.append("0");
-                }
-                a = s1.toString() + a;
-            } 
-            // 存储进位
-            int digit = 0;
-            // 存储返回结果
             StringBuffer result = new StringBuffer();
-            // 存储a字符串中的某个字符的数字
+            // 定义二个栈存储数据
+            Stack<Integer> aStack = new Stack<>();
+            Stack<Integer> bStack = new Stack<>();
+            for (int i=0,len=a.length();i<len;i++) {
+                aStack.push(Integer.valueOf(String.valueOf(a.charAt(i))));
+            }
+            for (int i=0,len=b.length();i<len;i++) {
+                bStack.push(Integer.valueOf(String.valueOf(b.charAt(i))));
+            }
             int aInt = 0;
-            // 存储b字符串中的某个字符的数字
             int bInt = 0;
-            for (int i=max-1;i>=0;i--) {
-                aInt = Integer.parseInt(String.valueOf(a.charAt(i)));
-                bInt = Integer.parseInt(String.valueOf(b.charAt(i)));
-                result.append(String.valueOf((aInt + bInt + digit)%2));
-                digit = (aInt + bInt + digit)/2;
+            int digit = 0;
+            while(!aStack.empty() || !bStack.empty()) {
+                //
+                if (!aStack.empty()) {
+                    aInt = aStack.pop();
+                } else {
+                    aInt = 0;
+                }
+                if (!bStack.empty()) {
+                    bInt = bStack.pop();
+                } else {
+                    bInt = 0;
+                }
+                result.append((aInt + bInt + digit)%2);
+                digit = (aInt + bInt + digit) / 2;
             }
             // 如果最后的进位为1的话，也要加到结果集中
             if (digit == 1) {
