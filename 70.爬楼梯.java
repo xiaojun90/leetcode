fbf8bca7c2eacab9a40f502df3b926f8e26de6ad
xiaoj <838249367@qq.com>
2019-03-16 00:39:29 +0800

    [83] 删除排序链表中的重复元素

---
+++ 70.爬楼梯.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (44.62%)
+ * Total Accepted:    34.6K
+ * Total Submissions: 77.5K
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 注意：给定 n 是一个正整数。
+ * 
+ * 示例 1：
+ * 
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 
+ * 示例 2：
+ * 
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * 
+ * 
+ */
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        // 斐波那契数列
+        int i = 1;
+        int j = 2;
+        int count = 0;
+        int k =3;
+        while(k<=n) {
+            count = i + j;
+            i = j;
+            j = count;
+            k ++;
+        }
+        return count;
+    }
+}
+
